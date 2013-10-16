72470f8e614989656b82a967b27fcad864b4dd97
Sasha Gerrand <sasha.gerrand@bigcommerce.com>
2013-10-17 10:56:46 +1100

    Removed extra whitespace

--- spec/integration/orders_spec.rb
+++ spec/integration/orders_spec.rb
@@ -7,12 +7,12 @@ describe "/api/v2/orders" do
     orders = api.get_orders
     orders.should be_a_kind_of(Enumerable)
     orders.size.should be > 0
-  end   
+  end
 
   it "filters orders by date", :vcr do
     orders = api.get_orders_by_date('2013-03-01')
     orders.should be_a_kind_of(Enumerable)
     orders.size.should be > 0
   end
-  
+
 end
--- spec/unit/api_request_spec.rb
+++ spec/unit/api_request_spec.rb
@@ -1,6 +1,6 @@
 describe "API request delegation" do
   include_context "mock api"
-  
+
   it "requests a resource" do
     api.connection.should_receive(:get).once.with("/time")
     api.get_time
@@ -10,7 +10,7 @@ describe "API request delegation" do
     api.connection.should_receive(:get).once.with("/products/333", {})
     api.get_product(333)
   end
-  
+
   it "requests a compound resource by ids" do
     api.connection.should_receive(:get).once.with("/orders/999/products/333", {})
     api.get_orders_product(999, 333)
@@ -30,5 +30,5 @@ describe "API request delegation" do
     api.connection.should_receive(:get).once.with("/customers", {:limit => 10, :page => 2})
     api.get_customers(:limit => 10, :page => 2)
   end
-  
+
 end
