3179920759262746d5a99e595fa334b7def1721f
Justin Lambert <justin.lambert@bigcommerce.com>
2013-11-26 11:44:49 +1100

    dramatically faster brand calls using memo-ization

--- lib/bigcommerce/Product.rb
+++ lib/bigcommerce/Product.rb
@@ -1,5 +1,13 @@
 module Bigcommerce
   class Product < Resource
 
+    # Here we memoize brand, so we can re-use it across different products
+    # Note we store this across the class
+    def brand
+      key = @hash["brand"]["resource"]
+      @@some_method ||= {}
+      @@some_method[key] ||= @connection.get(@hash["brand"]["resource"])
+    end
+
   end
 end
