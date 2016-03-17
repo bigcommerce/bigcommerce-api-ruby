4fb427dc1dc1905d66f4bd524f0d80be085c884c
Mark Garringer <mark@thehubmedia.com>
2016-03-17 17:11:26 -0400

    Added params to the Customer count call

--- spec/bigcommerce/unit/resources/customers/customer_spec.rb
+++ spec/bigcommerce/unit/resources/customers/customer_spec.rb
@@ -4,9 +4,10 @@ describe Bigcommerce::Customer do
   before(:each) { @customer = Bigcommerce::Customer }
 
   describe '.count' do
-    it 'should hit the correct path' do
-      expect(@customer).to receive(:get).with('customers/count')
-      @customer.count
+    it 'should hit the correct path and should allow params' do
+      params = { page: 1 }
+      expect(@customer).to receive(:get).with('customers/count', params)
+      @customer.count params
     end
   end
 end
